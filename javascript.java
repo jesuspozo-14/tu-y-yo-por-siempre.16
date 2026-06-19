5197c514ec9be03919a7c93609c01b2b895bc3cd
jesus <jpozo1404@gmail.com>
2026-06-18 19:58:36 -0500

    Add files via upload

---
+++ javascript.java
@@ -0,0 +1,6 @@
+document.getelementById('verImagenes').onclink=funtion() {
+    window.location.href='mis-imagenes.html';
+}
+document.getelementById("verImagenes").addEventListener("clik",fution(){
+    window.location.href="galeria.html";
+});
