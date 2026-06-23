7bac27343eb45a22c9a9eaa7e66349953181c75a
jesus <jpozo1404@gmail.com>
2026-06-23 16:25:15 -0500

    Delete 16 directory

--- 16/javascript.java
+++
@@ -1,6 +0,0 @@
-document.getelementById('verImagenes').onclink=funtion() {
-    window.location.href='mis-imagenes.html';
-}
-document.getelementById("verImagenes").addEventListener("clik",fution(){
-    window.location.href="galeria.html";
-});
