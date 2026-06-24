a047e7120e97e2edb2f913c33bdf60fa6c741ba0
jesus <jpozo1404@gmail.com>
2026-06-23 19:04:12 -0500

    Add files via upload

--- javascript.java
+++ javascript.java
@@ -4,3 +4,9 @@ document.getelementById('verImagenes').onclink=funtion() {
 document.getelementById("verImagenes").addEventListener("clik",fution(){
     window.location.href="galeria.html";
 });
+const frases = [
+    { tiempo: 19, texto: "quiero que sepas que vivo por ti" },
+    { tiempo: 24, texto: "nadie ocupara tu lugar" },
+    { tiempo: 30, texto: "desperte con el miedo de perderte" },
+    { tiempo: 34, texto: "me equivoque y de eso soy consiente" },
+];
